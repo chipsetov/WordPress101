3a9ae713acec12e3c750fe8c430d95dfc29892b8
bestproject <bestproject049@gmail.com>
2017-09-13 18:12:59 +0300

    create post formats

--- wp-content/themes/customtheme/index.php
+++ wp-content/themes/customtheme/index.php
@@ -2,14 +2,10 @@
 
 	<?php 
 	if(have_posts() ):
-		while ( have_posts()): the_post();?>
+		while ( have_posts()): the_post(); ?>
 
-			<h3><?php the_title(); ?></h3>
-			<div class="thumbnail-img"><?php the_post_thumbnail('thumbnail'); ?></div>
-			<small>Posted on: <?php the_time('F j, Y'); ?> at <?php the_time('g:i a'); ?>, in <?php the_category(); ?></small>
+			<?php get_template_part('content', get_post_format()); ?>
 
-			<p><?php the_content(); ?></p>
-			<hr>
 
 	<?php	endwhile;
 
